10592796b9e1d789b7f010bc8adb7ec4e84ff722
pjm529 <pjm529529@gmail.com>
2025-02-18 10:44:28 +0900

    Refactor: firebase 설정 공통

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,20 @@
+// Firebase SDK 라이브러리 가져오기
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+// Firebase 구성 정보 설정
+const firebaseConfig = {
+    apiKey: "AIzaSyA5EuVLjMcY3IWMo7o_meR7_my34oQUb54",
+    authDomain: "sparta-study-cac49.firebaseapp.com",
+    projectId: "sparta-study-cac49",
+    storageBucket: "sparta-study-cac49.firebasestorage.app",
+    messagingSenderId: "25693633721",
+    appId: "1:25693633721:web:836a552b6b59c302a6ef68"
+};
+
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 필요한 객체들을 export
+export { app, db, collection, addDoc, getDocs, doc, updateDoc, deleteDoc };
